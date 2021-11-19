9d6cb4d75fef15917564ba971d05f1b20ddb196c
Yichen Xu <yichen.x@outlook.com>
2021-11-19 11:25:47 +0800

    Cleanup

--- src/Definitions.v
+++ src/Definitions.v
@@ -921,26 +921,6 @@ G ⊢ {A: S1..T1} <: {A: S2..T2}
     G ⊢ typ_rcd { A >: S1 <: T1 } <: typ_rcd { A >: S2 <: T2 }
 
 (** [[
-G ⊢ {A: S1..T1} <: {A: S2..T2}
-_________________
-G ⊢ S2 <: S1
-]]
-*)
-(* | subtyp_typ_inv1: forall G S1 S2 T1 T2 A, *)
-(*     G ⊢ typ_rcd { A >: S1 <: T1 } <: typ_rcd { A >: S2 <: T2 } -> *)
-(*     G ⊢ S2 <: S1 *)
-
-(** [[
-G ⊢ {A: S1..T1} <: {A: S2..T2}
-_________________
-G ⊢ T1 <: T2
-]]
-*)
-(* | subtyp_typ_inv2: forall G S1 S2 T1 T2 A, *)
-(*     G ⊢ typ_rcd { A >: S1 <: T1 } <: typ_rcd { A >: S2 <: T2 } -> *)
-(*     G ⊢ T1 <: T2 *)
-
-(** [[
 G ⊢ U1 <: U2
 U1 ↘ {A: S1..T1}
 U2 ↘ {A: S2..T2}
