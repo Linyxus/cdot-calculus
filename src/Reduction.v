39df89d3db1552bb007c325dc38f5b813b8fbfab
Yichen Xu <yichen.x@outlook.com>
2022-04-07 11:45:32 +0800

    add new reduction rules

--- src/Reduction.v
+++ src/Reduction.v
@@ -5,22 +5,42 @@ Require Import Definitions Lookup Sequences.
 
 (** * Operational Semantics *)
 
-Definition matched_case γ p q A :=
-  exists r T ds r0, γ ⟦ defp p ⤳* defv (ν[r↘A](T)ds) ⟧ /\  (* checks that runtime value of the path is matchable *)
-                   γ ⟦ defp (open_path_p p r) ⤳* defp r0 ⟧ /\
-                   γ ⟦ defp q ⤳* defp r0 ⟧.
+Definition resolved_path γ p :=
+  exists v, γ ⟦ p ⤳ defv v ⟧.
 
 (** Term-reduction relation *)
 Reserved Notation "t1 '⟼' t2" (at level 40, t2 at level 39).
 
 Inductive red : sta * trm -> sta * trm -> Prop :=
 
-(** [γ ⊢ q ⤳* λ(T)t ]      #<br>#
+(** [γ ⊢ p ⤳ q ]      #<br>#
+    [―――――――――――――――――――――]      #<br>#
+    [γ | p ⟼ γ | q]      *)
+| red_resolve: forall γ p q,
+    γ ⟦ p ⤳ defp q ⟧ ->
+    (γ, trm_path p) ⟼ (γ, trm_path q)
+
+(** [γ ⊢ q ⤳ λ(T)t ]      #<br>#
     [―――――――――――――――――――――]      #<br>#
     [γ | q p ⟼ γ | tᵖ]      *)
 | red_app: forall γ p q T t,
-    γ ⟦ defp q ⤳* defv (λ(T) t) ⟧ ->
+    γ ⟦ q ⤳ defv (λ(T) t) ⟧ ->
     (γ, trm_app q p) ⟼ (γ, open_trm_p p t)
+
+(** [γ | q ⟼ q' ]      #<br>#
+    [―――――――――――――――――――――]      #<br>#
+    [γ | q p ⟼ γ | q' p]      *)
+| red_ctx_app1: forall γ p q q',
+    (γ, trm_path q) ⟼ (γ, trm_path q') ->
+    (γ, trm_app q p) ⟼ (γ, trm_app q' p)
+
+(** [γ | p ⟼ p' ]      #<br>#
+    [―――――――――――――――――――――]      #<br>#
+    [γ | ρ^γ p ⟼ γ | ρ^γ p']      *)
+| red_ctx_app2: forall γ p p' q,
+    resolved_path γ q ->
+    (γ, trm_path p) ⟼ (γ, trm_path p') ->
+    (γ, trm_app q p) ⟼ (γ, trm_app q p')
 
 (** [γ | let x = v in t ⟼ γ, x = v | tˣ] *)
 | red_let_val : forall v t γ x,
@@ -29,6 +49,7 @@ Inductive red : sta * trm -> sta * trm -> Prop :=
 
 (** [γ | let y = p in t ⟼ γ | tᵖ] *)
 | red_let_path : forall t γ p,
+    resolved_path γ p ->
     (γ, trm_let (trm_path p) t) ⟼ (γ, open_trm_p p t)
 
 (** [γ | t0 ⟼ γ' | t0']                            #<br>#
@@ -38,19 +59,50 @@ Inductive red : sta * trm -> sta * trm -> Prop :=
     (γ, t0) ⟼ (γ', t0') ->
     (γ, trm_let t0 t) ⟼ (γ', trm_let t0' t)
 
-(** [γ ⊢ p ⤳* ν[q.A](x: T)ds ]      #<br>#
+(** [γ ⊢ p ⤳ ν[q.A](x: T)ds ]      #<br>#
+    [γ ⊢ q ⤳* ρ^γ ]      #<br>#
     [―――――――――――――――――――――]      #<br>#
-    [γ | case p of tag q.A y => t1 | else => t2 ⟼ γ | t1^p ]      *)
-| red_case_match : forall γ p q A t1 t2,
-    matched_case γ p q A ->
-    (γ, trm_case p q A t1 t2) ⟼ (γ, open_trm_p p t1)
+    [γ | case p of y: (ρ^γ).A y => t1 else t2 ⟼ γ | t1^p ]      *)
+| red_case_match : forall γ p q A T ds r t1 t2,
+    resolved_path γ r ->
+    γ ⟦ p ⤳ defv (ν[q↘A](T)ds) ⟧ ->
+    γ ⟦ defp q ⤳* defp r ⟧ ->
+    (γ, trm_case p r A t1 t2) ⟼ (γ, open_trm_p p t1)
 
-(** [γ ⊢ p ⤳* tag r1.A r2 ]      #<br>#
+(** [γ ⊢ p ⤳ ν[q.A](x: T)ds ]      #<br>#
+    [γ ⊢ q ⤳* ρ₁^γ ]      #<br>#
+    [ρ₁ ≠ ρ₂]      #<br>#
     [―――――――――――――――――――――]      #<br>#
-    [γ | case p of tag q.A y => t1 | else => t2 ⟼ γ | t2 ]      *)
-| red_case_else : forall γ p q A t1 t2,
-    ~(matched_case γ p q A) ->
-    (γ, trm_case p q A t1 t2) ⟼ (γ, t2)
+    [γ | case p of y: (ρ₂^γ).A y => t1 else t2 ⟼ γ | t1^p ]      *)
+| red_case_else : forall γ p q A T ds r1 r2 t1 t2,
+    resolved_path γ r1 ->
+    resolved_path γ r2 ->
+    γ ⟦ p ⤳ defv (ν[q↘A](T)ds) ⟧ ->
+    γ ⟦ defp q ⤳* defp r1 ⟧ ->
+    r1 <> r2 ->
+    (γ, trm_case p r2 A t1 t2) ⟼ (γ, t2)
+
+(** [γ ⊢ p ⤳ λ(x: T)t ]      #<br>#
+    [―――――――――――――――――――――]      #<br>#
+    [γ | case p of y: r.A y => t1 else=> t2 ⟼ γ | t2 ]      *)
+| red_case_lambda : forall γ p t T r A t1 t2,
+    γ ⟦ p ⤳ defv (λ(T) t) ⟧ ->
+    (γ, trm_case p r A t1 t2) ⟼ (γ, t2)
+
+(** [γ ⊢ p ⤳ p' ]      #<br>#
+    [―――――――――――――――――――――]      #<br>#
+    [γ | case p of y: r.A y => t1 else t2 ⟼ γ | case p' of tag r.A y => t1 else t2 ]      *)
+| red_ctx_case1 : forall γ p p' r A t1 t2,
+    γ ⟦ p ⤳ defp p' ⟧ ->
+    (γ, trm_case p r A t1 t2) ⟼ (γ, trm_case p' r A t1 t2)
+
+(** [γ ⊢ r ⤳ r' ]      #<br>#
+    [―――――――――――――――――――――]      #<br>#
+    [γ | case ρ^λ of y: r'.A y => t1 else t2 ⟼ γ | case ρ^γ of tag r'.A y => t1 else t2 ]      *)
+| red_ctx_case2 : forall γ p r r' A t1 t2,
+    resolved_path γ p ->
+    γ ⟦ r ⤳ defp r' ⟧ ->
+    (γ, trm_case p r A t1 t2) ⟼ (γ, trm_case p r' A t1 t2)
 
 where "t1 '⟼' t2" := (red t1 t2).
 
@@ -60,8 +112,8 @@ Notation "t1 '⟼*' t2" := (star red t1 t2) (at level 40).
 (** ** Normal forms *)
 
 (** Paths and values are considered to be in normal form. *)
-Inductive norm_form: trm -> Prop :=
-| nf_path: forall p, norm_form (trm_path p)
-| nf_val: forall v, norm_form (trm_val v).
+Inductive norm_form: sta -> trm -> Prop :=
+| nf_path: forall γ p, (exists v, γ ⟦ p ⤳ defv v ⟧) -> norm_form γ (trm_path p)
+| nf_val: forall γ v, norm_form γ (trm_val v).
 
 Hint Constructors red norm_form.
